9777b694cc19b05f971c72ab9829d388f06000af
Mark Wallsgrove <mark.wallsgrove+git@gmail.com>
2017-07-30 12:49:31 +0100

    multiple build layers, api, react

--- webui/main.go
+++ webui/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/markwallsgrove/saml_federation_proxy/models"
 
 	mgo "gopkg.in/mgo.v2"
 )
@@ -10,23 +14,34 @@ import (
 type mongoHandler func(http.ResponseWriter, *http.Request, *mgo.Session)
 type handler func(http.ResponseWriter, *http.Request)
 
+func getInt(r *http.Request, name string, min int, max int, def int) int {
+	num, _ := strconv.Atoi(r.URL.Query().Get(name))
+
+	if num < min || num > max {
+		return def
+	}
+
+	return num
+}
+
+func apiEntityDescriptorHandler(w http.ResponseWriter, r *http.Request, mongoSession *mgo.Session) {
+	c := mongoSession.DB("fedproxy").C("entityDescriptors")
+	os := getInt(r, "offset", 0, 1000000, 0)
+	lmt := getInt(r, "limit", 1, 100, 10)
+
+	// TODO: skip & limit is slow with large amount of data
+	query := c.Find(nil).Sort("-_id").Skip(os).Limit(lmt)
+
+	var entityDescriptors []models.EntityDescriptor
+	if err := query.All(&entityDescriptors); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("500 - Something bad happened!"))
+	} else {
+		json.NewEncoder(w).Encode(entityDescriptors)
+	}
+}
+
 func index(w http.ResponseWriter, r *http.Request, mongoSession *mgo.Session) {
-	// c := mongoSession.DB("fedproxy").C("entityDescriptors")
-
-	// var entityDescriptors []models.EntityDescriptor
-	// query := c.Find(nil).Sort("-_id").Limit(10)
-
-	// if err := query.All(&entityDescriptors); err != nil {
-	// 	w.WriteHeader(http.StatusInternalServerError)
-	// 	w.Write([]byte("500 - Something bad happened!"))
-	// 	return
-	// }
-
-	// fmt.Fprintf(w, "Hi there, I love!")
-	// for _, entityDescriptor := range entityDescriptors {
-	// 	fmt.Fprintf(w, "<br /> entity descriptor: %s", entityDescriptor.EntityID)
-	// }
-
 	http.ServeFile(w, r, "/public/index.html")
 }
 
@@ -54,8 +69,9 @@ func main() {
 	defer session.Close()
 
 	fs := http.FileServer(http.Dir("/public"))
-	http.Handle("/public/", http.StripPrefix("/public/", fs))
 
+	http.Handle("/public/", http.StripPrefix("/public/", fs))
 	http.HandleFunc("/", htmlWrap(index, session))
+	http.HandleFunc("/1/api/entitydescriptor", jsonWrap(apiEntityDescriptorHandler, session))
 	http.ListenAndServe(":8080", nil)
 }
--- webui/src/App.js
+++ webui/src/App.js
@@ -1,6 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-const App = (props) => (<h1>Hello, World!</h1>)
+function paginateEntityDescriptors() {
+    return axios.get('/1/api/entitydescriptor').then(resp => {
+        // TODO: error?
+        return resp;
+    });
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            entitydescriptors: [],
+        };
+    }
+
+    componentDidMount() {
+        paginateEntityDescriptors().then(resp => {
+            this.setState({entitydescriptors: resp.data});
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, Toast cat!</h1>
+                <ul>
+                    {this.state.entitydescriptors.map(ed => {
+                        return <li>name: {ed.EntityID}</li>
+                    })}
+                </ul>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(<App />,  document.getElementById('app'))
