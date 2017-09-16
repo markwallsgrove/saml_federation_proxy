ecc7230a3164bb9dcf05be20269e977ca0612a19
Mark Wallsgrove <mark.wallsgrove+git@gmail.com>
2017-09-16 20:11:23 +0100

    split web ui & api into separate containers

--- webapi/main.go
+++ webapi/main.go
@@ -196,7 +196,7 @@ func main() {
 
 	srv := &http.Server{
 		Handler:      r,
-		Addr:         "0.0.0.0:8080",
+		Addr:         "0.0.0.0:80",
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
